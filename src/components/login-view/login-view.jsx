43b66b70b3496f7f872511adc7fd8d14c9f0ff9b
Darisam <98291008+Darisam@users.noreply.github.com>
2022-05-04 12:04:39 +0200

    Cosmetic changes

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -8,46 +8,48 @@ export function LoginView(props) {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(username, password);
-    // To Do: Authenticate User, then register user as logged in
+    // To Do: Authenticate User
     props.onLoggedIn(username);
   };
 
   return (
     <div>
-      <div>
-        <form>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-          <br />
-          <label htmlFor="password">Password:</label>
-          <input
-            type="text"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
-          />
-          <button type="submit" onClick={handleSubmit}>
-            Submit
-          </button>
-        </form>
-      </div>
-      <div>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          autoFocus
+          value={username}
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="text"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <br />
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+
+      <p>
         Not registered?
+        <br />
         <button type="button" onClick={props.onUnregistered}>
           Register Now
         </button>
-      </div>
+      </p>
     </div>
   );
 }
